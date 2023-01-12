5ba2609c7b37f985c2229b51cf43b67e70c68cae
Raymond Johnson <rj740141@southeast.edu>
2023-01-12 13:47:47 -0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <TextInput>Enter</TextInput>
+      <Text>Thingy</Text>
+      <Text>wow</Text>
       <StatusBar style="auto" />
     </View>
   );
